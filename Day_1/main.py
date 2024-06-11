43f69ac411127ae76f3905ad55cb0fd6ef27dc47
Koi <61529257+KoiFish97@users.noreply.github.com>
2024-06-11 16:45:16 -0700

    Day 1 complete

---
+++ Day_1/main.py
@@ -0,0 +1,69 @@
+# print("3 methods to quoting inside a print statement:")
+# print("A 'single quote' inside a double quote")
+# print('A "double quote" inside a single quote')
+# print("Alternatively you can just \"escape\" the quote\n")
+
+# print("Spaces:")
+# print("Hello" + "Koi")
+# print("Hello " + "Koi")
+# print("Hello" + " Koi")
+# print("Hello" + " " + "Koi\n")
+
+# input("What is your name?")
+# input() will get user input in console
+# print() will print the word "Hello" and the input
+# print("Hello " + input("What is your name?"))
+
+# print("Day 1 - Printing")
+# print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+# print("2. Knead the dough for 10 minutes.")
+# print("3. Add 3g of Salt.")
+# print("4. Leave to rise for 2 hours.")
+# print("5. Bake at 200 degrees C for 30 minutes.\n")
+
+# Fix 1
+# print(Day 1 - String Manipulation")
+# print("Day 1 - String Manipulation")
+
+# Fix 2
+# print("String Concatenation is done with the "+" sign.")
+# print('String Concatenation is done with the "+" sign.')
+
+# Fix 3
+#   print('e.g. print("Hello " + "world")')
+# print('e.g. print("Hello " + "world")')
+
+# Fix 4
+# print(("New lines can be created with a backslash and n.")
+# ("New lines can be created with a backslash and n.\n")
+
+# print("Day 1 - Input Function")
+# name1 and name2 are strings with an input from a user
+# name1 = str(input())
+# name2 = str(input())
+
+# print("I go by " + name1 + " or " + name2)
+
+# nameLetters takes an input and len() gets length of input
+# nameLetters = len(input())
+
+# print(nameLetters)
+
+# name = input()
+# print(len(name)
+
+# Shorter Version(Solution)
+# print(len(input()))
+
+# print(Day 1 - Variables)
+# Variable Swapping
+
+# a = input()
+# b = input()
+
+# c = a
+# a = b
+# b = c
+
+# print("a: " + a)
+# print("b: " + b)
