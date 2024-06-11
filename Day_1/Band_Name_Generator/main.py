1ca5a3ed8bbd8f8f05f48a97a7dcf24970576fa3
Koi <61529257+KoiFish97@users.noreply.github.com>
2024-06-11 16:28:41 -0700

    Day 1 complete

---
+++ Day_1/Band_Name_Generator/main.py
@@ -0,0 +1,7 @@
+print("Welcome! Enter responses to prompts for a Band Name!")
+
+city = input("What city did you grow up in?\n")
+
+pet_name = input("What is your pets name?\n")
+
+print("Your band name is: " + city + " " + pet_name)
