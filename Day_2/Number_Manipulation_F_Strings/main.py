5af4340198af694cbf4e0f2fe090f01b7d6042cd
Koi <61529257+KoiFish97@users.noreply.github.com>
2024-06-12 05:35:48 -0700

    Day 2 Part 3 Complete

---
+++ Day_2/Number_Manipulation_F_Strings/main.py
@@ -0,0 +1,46 @@
+# Rounding
+# print(round(8 / 3)) = 3
+
+# Specify the number of digit precision
+# In this case, 2 decimal places
+# print(round(8 / 3, 2)) = 2.67
+
+# Floor Division
+# Works like int conversion chopping off all decimal values
+# Puts out an integer as opposed to float like most division
+# print(8 // 3) = 2
+
+# Saving Results
+# Can then further do math on the variable
+# result = 4 / 2
+# result /= 2
+# print(result)
+# 4 / 2 = 2 / 2 = 1
+
+# Score example
+# score = 0
+# score = score + 1 (Not preferred)
+# score += 1 (Changes score to 1)
+# print(score)
+
+# F Strings
+# score = 0
+# print("your score is " + score)
+# ^^^ Creates an error due to different data types
+
+# F Strings convert variables for you
+# score = 0
+# height = 1.8
+# isWinning = True
+# print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
+
+# Day 2 - Life in Weeks
+# My Solution (Matched Challenge Solution)
+# age = input()
+# yearsLeft = 90 - int(age)
+# weeksLeft = yearsLeft * 52
+# print(f"You have {weeksLeft} weeks left.")
+
+
+
+
