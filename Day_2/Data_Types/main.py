1fc834d7a390b7a74ec30c1851be0370afc95e81
Koi <61529257+KoiFish97@users.noreply.github.com>
2024-06-11 20:48:45 -0700

    Day 2 Part 1

---
+++ Day_2/Data_Types/main.py
@@ -0,0 +1,17 @@
+# Data Types
+
+# String
+# Square brackets index's a number from a string
+# print("Hello"[4])
+
+# Integer
+# No quotes on numbers will be seen as integers
+# print(123 + 345)
+# Python ignores underscores thus they can be used for readability
+# print(123_456_789)
+
+# Float
+# 3.14159
+
+# Boolean
+# True or False
