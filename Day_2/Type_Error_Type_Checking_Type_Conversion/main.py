4984f543b666a5635c6d302b8dd6de3f5be8d85f
Koi <61529257+KoiFish97@users.noreply.github.com>
2024-06-11 21:04:13 -0700

    Day 2 Part 2

---
+++ Day_2/Type_Error_Type_Checking_Type_Conversion/main.py
@@ -0,0 +1,37 @@
+# Type Errors
+
+# Creates Error
+# num_char = len(input("What is your name?"))
+# print("Your name has " + num_char + "characters.")
+
+# Prints the data type of what you put in (here)
+# print(type(num_char))
+# print(type(len(input("What is your name?"))))
+
+# num_char = len(input("What is your name?"))
+
+# String Conversion
+# new_num_char = str(num_char)
+# print("Your name has " + new_num_char + " characters.")
+
+# Integer
+# a = 123
+# print(type(a))
+
+# String
+# a = str(123)
+# print(type(a))
+
+# Float
+# a = float(123)
+# print(type(a))
+
+# Float math number
+# print(70 + float(100.5))
+
+# Prints string
+# print(str(70)+ str(100))
+
+
+
+
