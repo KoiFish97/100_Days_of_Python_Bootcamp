6c05a73f4c04e6fafb9fcb0f469a4fe760b5092f
Koi <61529257+KoiFish97@users.noreply.github.com>
2024-06-13 07:51:45 -0700

    Day 4 Part 1

---
+++ Day_4/Randomisation/main.py
@@ -0,0 +1,27 @@
+import random
+# import my_module
+# print(my_module.pi)
+
+# Random Module
+# Ask python to find random module
+# random_integer = random.randint(1, 10)
+# print(random_integer)
+
+# prints a random number from 0.0000 - 1
+# random_float = random.random()
+# print(random_float)
+
+# multiplies the random number by 5 increasing the range
+# print(random_float * 5)
+
+# love_score = random.randint(1, 100)
+# print(f"Your love score is {love_score}")
+
+# Day_4 - Heads or Tails
+# import random
+
+# heads_tails = random.randint(0, 1)
+# if heads_tails == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
