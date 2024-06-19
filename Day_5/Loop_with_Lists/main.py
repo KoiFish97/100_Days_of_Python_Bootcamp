029902eb708e93552e42766132305c33401f4bc7
Koi <61529257+KoiFish97@users.noreply.github.com>
2024-06-18 20:26:39 -0700

    Day 5 part 1 Complete

---
+++ Day_5/Loop_with_Lists/main.py
@@ -0,0 +1,39 @@
+# For Loop
+# for item in list_of_items:
+#   Do something
+# fruits = ["Apple", "Peach", "Pear"]
+# for fruit in fruits:
+#     print(fruit)
+#     print(fruit + "Pie")
+# print(fruits)
+
+# Day 5 - Average Height
+# # Input a Python list of student heights
+# student_heights = input().split()
+# for n in range(0, len(student_heights)):
+#   student_heights[n] = int(student_heights[n])
+
+# total_height = 0
+# for heights in student_heights:
+#   total_height += heights
+# print(f"total height = {total_height}")
+
+# total_students = 0
+# for students in student_heights:
+#   total_students += 1
+# print(f"number of students = {total_students}")
+
+# average_height = round(total_height / total_students)
+# print(f"average height = {average_height}")
+
+# Day 5 - High Score
+# Input a list of student scores
+# student_scores = input().split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+
+# highest_score = 0
+# for scores in student_scores:
+#   if scores >= highest_score:
+#     highest_score = scores
+# print(f"The highest score in the class is: {highest_score}")
