107168f51cd36edc7710ac47217d2df0bebf9f36
Koi <61529257+KoiFish97@users.noreply.github.com>
2024-06-20 16:57:56 -0700

    Day 6 part 1

---
+++ Day_6/Defining_and_Calling_Functions/main.py
@@ -0,0 +1,18 @@
+# Functions
+# Built in Functions - https://docs.python.org/3/library/functions.html
+
+# print("Hello") Prints whats in ()
+# len("Hello") gets length of whats in ()
+
+# num_char = len("Hello")
+# print(num_char)
+
+# Creating a function
+
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+# my_function()
+
+# Reeborg's World - https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Alone&url=worlds%2Ftutorial_en%2Falone.json
+
