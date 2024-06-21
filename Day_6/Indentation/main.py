c3f7d544edea4799367fc998b3e2b1c8bcb3a62c
Koi <61529257+KoiFish97@users.noreply.github.com>
2024-06-21 02:23:59 -0700

    Day 6 part 2 and challenge complete

---
+++ Day_6/Indentation/main.py
@@ -0,0 +1,17 @@
+# Indentation
+
+# def my_function():
+#   any actions within a function are indented 4 spaces or 1 tab
+# print("Hello")
+
+# More complicated indentation
+# def my_function():
+#     if sky == "clear":
+#         print("blue")
+#     elif sky == "cloudy":
+#         print("grey")
+#     print("Hello")
+
+
+# print("World")
+
---
+++ Day_6/While_Loops/main.py
@@ -0,0 +1,68 @@
+# While Loops
+
+# while something_is_true:
+#     do this
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# def jump():
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+
+# for step in range(6):
+#     jump()
+
+# while at_goal() == False:
+#     jump()
+
+# while not at_goal():
+#     jump()
+
+# while front_is_clear() and not at_goal():
+#    move()
+#    while wall_in_front() and not at_goal():
+#        jump()
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
+# def jump():
+#     turn_left()
+
+#     while wall_on_right():
+#         move()
+#     turn_right()
+#     move()
+#     turn_right()
+
+#     while front_is_clear():
+#         move()
+#     turn_left()
+
+
+# while not at_goal():
+#     if wall_in_front():
+#         jump()
+#     else:
+#         move()
